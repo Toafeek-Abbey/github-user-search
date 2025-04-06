f97d9a1fd64aec6521f20e9e566ff3aa437f9944
Taofeek Ayuba <taofeekayubaabiodun@gmail.com>
2025-04-06 20:30:38 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import Landing from "./pages/Landing"
+import About from "./pages/About"
+
+function App() {
+  return (
+    <>
+      
+      <h1>Welcome</h1>
+      <Landing />
+      <About />
+    </>
+  )
+}
+
+export default App
